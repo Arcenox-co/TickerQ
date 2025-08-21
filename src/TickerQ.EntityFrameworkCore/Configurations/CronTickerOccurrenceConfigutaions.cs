ca1c1d5dfb5f9621dbebbec08897d5d01918403d
Cédric Luthi <cedric.luthi@gmail.com>
2025-08-21 14:52:09 +0200

    Target .NET 8 and .NET 9
    Along with many related improvements.
    
    * No need to pass explicit PackageVersion property when building the projects since the version is now defined in `src/Directory.Build.props` instead of the `VERSION` file
    * No need to setup older .NET SDKs in the GitHub actions
    * Use `ProjectReference` instead of `PackageReference` so that changes across projects can be easily tested
    * Use a [FrameworkReference](https://learn.microsoft.com/en-us/aspnet/core/fundamentals/target-aspnetcore) instead of depending on the [deprecated](https://github.com/dotnet/aspnetcore/issues/60060) `Microsoft.AspNetCore.Http.Abstractions` package
    * Remove many unnecessary package references
    * Remove support code for ASP.NET Core < 3.1 (NetTargetV31Lower)
    * Use the more natural version range format "1.2.3" instead of "[1.2.3,)" which are equivalent
    * Drop the EnumerableExtension class. The [DistinctBy](https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.distinctby) method exists since .NET 6 and the Batch methods exists as the [Chunk](https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.chunk) method since .NET 6 too
    * Update to EF Core 8 and fix obsolete warnings (HasName → HasDatabaseName)
    * Update test dependencies to their latest versions
    * Adapt the TickerQ.WithDashboard sample code to use the new `AddDashboard` method

--- src/TickerQ.EntityFrameworkCore/Configurations/CronTickerOccurrenceConfigutaions.cs
+++ src/TickerQ.EntityFrameworkCore/Configurations/CronTickerOccurrenceConfigutaions.cs
@@ -22,13 +22,13 @@ namespace TickerQ.EntityFrameworkCore.Configurations
                 .IsRequired(false);
 
             builder.HasIndex("CronTickerId")
-                .HasName("IX_CronTickerOccurrence_CronTickerId");
+                .HasDatabaseName("IX_CronTickerOccurrence_CronTickerId");
 
             builder.HasIndex("ExecutionTime")
-                .HasName("IX_CronTickerOccurrence_ExecutionTime");
+                .HasDatabaseName("IX_CronTickerOccurrence_ExecutionTime");
 
             builder.HasIndex("Status", "ExecutionTime")
-                .HasName("IX_CronTickerOccurrence_Status_ExecutionTime");
+                .HasDatabaseName("IX_CronTickerOccurrence_Status_ExecutionTime");
 
             builder.HasOne(x => x.CronTicker)
                 .WithMany()
@@ -37,7 +37,7 @@ namespace TickerQ.EntityFrameworkCore.Configurations
 
             builder.HasIndex("CronTickerId", "ExecutionTime")
                 .IsUnique()
-                .HasName("UQ_CronTickerId_ExecutionTime");
+                .HasDatabaseName("UQ_CronTickerId_ExecutionTime");
 
             builder.ToTable("CronTickerOccurrences", _schema);
         }
