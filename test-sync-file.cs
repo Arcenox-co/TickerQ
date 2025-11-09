510e21663d1e0f9b5b8ccaeab18e4c1792198be0
Arcenox <albert@arcenox.com>
2025-11-09 22:30:11 +0100

    test

--- test-sync-file.cs
+++
@@ -1,9 +0,0 @@
-// Test file to demonstrate branch sync
-// This file is added to main and should be synced to net8
-namespace Test
-{
-    public class TestSync
-    {
-        public string Message => "This change is from main branch";
-    }
-}
