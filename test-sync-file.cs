341d8559a7a552f3b1376a4f2323224b63520bbd
Arcenox <albert@arcenox.com>
2025-11-09 22:28:40 +0100

    Add test file and exclude workflow files from PR sync

---
+++ test-sync-file.cs
@@ -0,0 +1,9 @@
+// Test file to demonstrate branch sync
+// This file is added to main and should be synced to net8
+namespace Test
+{
+    public class TestSync
+    {
+        public string Message => "This change is from main branch";
+    }
+}
