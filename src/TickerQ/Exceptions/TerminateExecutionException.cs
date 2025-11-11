dc91f76f668581aa409bbf37ac18e104cec34b8d
Albert Kunushevci <120019342+arcenox@users.noreply.github.com>
2025-11-12 00:03:11 +0100

    Extended Termination of a job with detailed and also with adjustable … (#349)
    * Extended Termination of a job with detailed and also with adjustable status
    
    * updated to default set skipped on variable

--- src/TickerQ/Exceptions/TerminateExecutionException.cs
+++ src/TickerQ/Exceptions/TerminateExecutionException.cs
@@ -1,10 +1,16 @@
 using System;
+using TickerQ.Utilities.Enums;
 
 namespace TickerQ.Exceptions
 {
     internal class TerminateExecutionException : Exception
     {
+        internal readonly TickerStatus Status = TickerStatus.Skipped;
         public TerminateExecutionException(string message) : base(message) { }
+        public TerminateExecutionException(TickerStatus tickerType, string message) : base(message) {}
+        public TerminateExecutionException(string message, Exception innerException) : base(message, innerException) {}
+        public TerminateExecutionException(TickerStatus tickerType, string message, Exception innerException) : base(message, innerException)
+            => Status = tickerType;
     }
 
     internal class ExceptionDetailClassForSerialization
