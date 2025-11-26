0a5cbc7b8af9d11fe2ebfb91789805c48f05bb65
Daniel Nordström <daniel.mauritzson@gmail.com>
2025-11-26 10:26:32 +0000

    Refactor UseTickerQDbContext to simplify schema assignment logic

--- src/TickerQ.EntityFrameworkCore/EfCoreOptionBuilder.cs
+++ src/TickerQ.EntityFrameworkCore/EfCoreOptionBuilder.cs
@@ -25,8 +25,7 @@ namespace TickerQ.EntityFrameworkCore
         
         public TickerQEfCoreOptionBuilder<TTimeTicker, TCronTicker> UseTickerQDbContext<TDbContext>(Action<DbContextOptionsBuilder> optionsAction, string schema = null) where TDbContext : TickerQDbContext<TTimeTicker, TCronTicker>
         {
-            if(!string.IsNullOrEmpty(schema))
-                Schema = schema;
+            Schema = schema ?? Schema;
             
             ServiceBuilder.UseTickerQDbContext<TDbContext, TTimeTicker, TCronTicker>(this, optionsAction);
             return this;
