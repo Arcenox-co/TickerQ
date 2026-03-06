4ddd580f28cbaef096ecdbe655ca779720a00466
Ingmar Olmaru <48388318+Kedireng@users.noreply.github.com>
2026-03-07 00:51:47 +0200

    Fix UseApplicationDbContext implementation #498 (#590)
    * Fix UseApplicationDbContext implementation
    
    * Remove unnecessary comment
    
    * Move to async implementation
    
    * Fix tests
    
    * Add sample project for ApplicationDbContext usage

--- src/TickerQ.EntityFrameworkCore/DbContextFactory/TickerQDbContext.cs
+++ src/TickerQ.EntityFrameworkCore/DbContextFactory/TickerQDbContext.cs
@@ -1,5 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
 using TickerQ.EntityFrameworkCore.Configurations;
 using TickerQ.Utilities.Entities;
 
@@ -30,5 +36,98 @@ public class TickerQDbContext : TickerQDbContext<TimeTickerEntity, CronTickerEnt
 {
     public TickerQDbContext(DbContextOptions<TickerQDbContext> options) : base(options)
     {
+    }
+}
+
+internal interface ITickerDbContextFactory<TContext>
+    where TContext : DbContext
+{
+    ITickerDbContextSession<TContext> CreateSession();
+    Task<ITickerDbContextSession<TContext>> CreateSessionAsync(CancellationToken cancellationToken = default);
+}
+
+internal class TickerDbContextFactory<TContext> : ITickerDbContextFactory<TContext>
+    where TContext : DbContext
+{
+    private readonly IServiceProvider _sp;
+
+    public TickerDbContextFactory(IServiceProvider sp)
+    {
+        _sp = sp;
+    }
+
+    public ITickerDbContextSession<TContext> CreateSession()
+        => TickerDbContextSession<TContext>.Create(_sp);
+
+    public Task<ITickerDbContextSession<TContext>> CreateSessionAsync(CancellationToken cancellationToken = default)
+        => TickerDbContextSession<TContext>.CreateAsync(_sp, cancellationToken);
+}
+
+internal interface ITickerDbContextSession<TContext> : IDisposable
+    where TContext : DbContext
+{
+    TContext Context { get; }
+}
+
+internal class TickerDbContextSession<TContext> : ITickerDbContextSession<TContext>
+    where TContext : DbContext
+{
+    private readonly IServiceScope _scope;
+    private readonly TContext _context;
+
+    public TContext Context => _context;
+
+    private TickerDbContextSession(IServiceScope scope, TContext context)
+    {
+        _context = context;
+        _scope = scope;
+    }
+
+    internal static async Task<ITickerDbContextSession<TContext>> CreateAsync(IServiceProvider sp, CancellationToken cancellationToken)
+    {
+        // 1. Try to get the Factory (Highest Priority)
+        var factory = sp.GetService<IDbContextFactory<TContext>>();
+
+        if (factory != null)
+        {
+            return new TickerDbContextSession<TContext>(null, await factory.CreateDbContextAsync(cancellationToken));
+        }
+        else
+        {
+            // 2. Fallback: Create a manual scope (Standard AddDbContext)
+            var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+            var scope = scopeFactory.CreateScope();
+            return new TickerDbContextSession<TContext>(scope, scope.ServiceProvider.GetRequiredService<TContext>());
+        }
+    }
+
+    internal static ITickerDbContextSession<TContext> Create(IServiceProvider sp)
+    {
+        // 1. Try to get the Factory (Highest Priority)
+        var factory = sp.GetService<IDbContextFactory<TContext>>();
+
+        if (factory != null)
+        {
+            return new TickerDbContextSession<TContext>(null, factory.CreateDbContext());
+        }
+        else
+        {
+            // 2. Fallback: Create a manual scope (Standard AddDbContext)
+            var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+            var scope = scopeFactory.CreateScope();
+            return new TickerDbContextSession<TContext>(scope, scope.ServiceProvider.GetRequiredService<TContext>());
+        }
+    }
+
+    public void Dispose()
+    {
+        if (_scope != null)
+        {
+            _scope.Dispose();
+        }
+        else
+        {
+            _context.Dispose();
+        }
     }
 }
