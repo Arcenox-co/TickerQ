150b3bb76440f8ce622def14df69f13acb072c56
KonstantinKritsin <KonstantinKritsin@users.noreply.github.com>
2026-07-31 21:29:35 +0300

    Allow derived providers to materialize time ticker state

--- src/TickerQ.Utilities/Entities/TimeTickerEntity.cs
+++ src/TickerQ.Utilities/Entities/TimeTickerEntity.cs
@@ -12,29 +12,29 @@ namespace TickerQ.Utilities.Entities
     public class TimeTickerEntity<TTicker> : BaseTickerEntity where TTicker : TimeTickerEntity<TTicker>
     {
         [JsonInclude]
-        public virtual TickerStatus Status { get; internal set; }
+        public virtual TickerStatus Status { get; protected internal set; }
         [JsonInclude]
-        public virtual string LockHolder { get; internal set; }
+        public virtual string LockHolder { get; protected internal set; }
         public virtual byte[] Request { get; set; }
         public virtual DateTime? ExecutionTime { get; set; }
         [JsonInclude]
-        public virtual DateTime? LockedAt { get; internal set; }
+        public virtual DateTime? LockedAt { get; protected internal set; }
         [JsonInclude]
-        public virtual DateTime? ExecutedAt { get; internal set; }
+        public virtual DateTime? ExecutedAt { get; protected internal set; }
         [JsonInclude]
-        public virtual string ExceptionMessage { get; internal set; }
+        public virtual string ExceptionMessage { get; protected internal set; }
         [JsonInclude]
-        public virtual string SkippedReason { get; internal set; }
+        public virtual string SkippedReason { get; protected internal set; }
         [JsonInclude]
-        public virtual long ElapsedTime { get; internal set; }
+        public virtual long ElapsedTime { get; protected internal set; }
         public virtual int Retries { get; set; }
         [JsonInclude]
-        public virtual int RetryCount { get; internal set; }
+        public virtual int RetryCount { get; protected internal set; }
         public virtual int[] RetryIntervals { get; set; }
         [JsonInclude]
-        public virtual Guid? ParentId { get; internal set; }
+        public virtual Guid? ParentId { get; protected internal set; }
         [JsonIgnore]
-        public virtual TTicker Parent { get; internal set; }
+        public virtual TTicker Parent { get; protected internal set; }
         public virtual ICollection<TTicker> Children { get; set; } = new List<TTicker>();
         public virtual RunCondition? RunCondition { get; set; }
     }
